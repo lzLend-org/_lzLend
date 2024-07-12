c303b419cb477b291ddfb49af3b3629270a63d19
ahmedhamedaly <ahmedhamedaly19@gmail.com>
2024-07-13 00:03:36 +0200

    tests: added mock test token and testnet chains

---
+++ contracts/tests/Token.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Token is ERC20 {
+    constructor(
+        string memory _name,
+        string memory _symbol
+    ) ERC20(_name, _symbol) {
+        _mint(msg.sender, 10_000_000_000 ether);
+    }
+}
