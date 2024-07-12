ab17868e0c23ce98592bda8352d1a72a65d8cc02
ahmedhamedaly <ahmedhamedaly19@gmail.com>
2024-07-13 00:36:28 +0200

    feat: prep for oracle aggregator

--- contracts/Oracle.sol
+++ contracts/Oracle.sol
@@ -1,6 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-contract Oracle {
-  
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {OApp, MessagingFee, Origin} from "@layerzerolabs/lz-evm-oapp-v2/contracts/oapp/OApp.sol";
+import {MessagingReceipt} from "@layerzerolabs/lz-evm-oapp-v2/contracts/oapp/OAppSender.sol";
+
+contract Oracle is OApp {
+    constructor(
+        address _endpoint,
+        address _delegate
+    ) OApp(_endpoint, _delegate) Ownable(_delegate) {}
+
+    function _lzReceive(
+        Origin calldata /*_origin*/,
+        bytes32 /*_guid*/,
+        bytes calldata payload,
+        address /*_executor*/,
+        bytes calldata /*_extraData*/
+    ) internal override {
+        string memory data = abi.decode(payload, (string));
+    }
 }
